88c5caadb3b69e32b5dd4570f5adc70520e9ce09
cmj <chmin82@gmail.com>
2026-01-17 10:33:38 +0900

    환경 변수 출력 기능 추가
    - `@Value` 어노테이션을 사용하여 `my-env` 값 주입
    - 홈 화면 출력 메시지에 환경 변수 값 추가

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${my-env}")
+    private String myEnv;
+
     @GetMapping("/")
     public String home() {
-        return "hello world 60 try 만에 성공!";
+        return "hello world 60 try 만에 성공! current env: " + myEnv;
     }
 
 }
